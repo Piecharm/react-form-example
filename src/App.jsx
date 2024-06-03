81246f0f70fc37d94caf33b0d7177782c973252d
Piecharm <deeprockz93@gmail.com>
2024-06-03 22:01:49 +0530

    added login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
 import "./App.css";
+import Login from "./components/Login";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
-            <div>hello</div>
+            <Login />
         </>
     );
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+const Login = () => {
+    const initialUserDetails = {
+        firstname: "",
+        lastname: "",
+        age: 0,
+    };
+
+    const [userDetails, setUserDetails] = useState(initialUserDetails);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setUserDetails({
+            ...userDetails,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(userDetails);
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>First name: </label>
+                <input
+                    type="text"
+                    placeholder="First name"
+                    value={userDetails.firstname}
+                    name="firstname"
+                    onChange={handleChange}
+                />
+
+                <label>Last name: </label>
+                <input
+                    type="text"
+                    placeholder="Last name"
+                    value={userDetails.lastname}
+                    name="lastname"
+                    onChange={handleChange}
+                />
+                <label>Age: </label>
+                <input
+                    type="number"
+                    placeholder="Age"
+                    value={userDetails.age}
+                    name="age"
+                    onChange={handleChange}
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
