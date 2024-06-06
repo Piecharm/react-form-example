e9df190dac442f6a88e6e3de02d9bb1bca0c42c8
Piecharm <deeprockz93@gmail.com>
2024-06-06 18:37:39 +0530

    navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,9 @@ const Navbar = () => {
         <nav className="bg-teal-900 border-gray-200 text-2xl font-semibold whitespace-nowrap text-white">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <div className="flex items-center space-x-3 rtl:space-x-reverse self-center hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-teal-700 md:p-0 md:dark:hover:text-teal-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                    <Link to="/">Home</Link>
+                    <Link className="focus:text-teal-200" to="/">
+                        Home
+                    </Link>
                 </div>
                 <div
                     className="hidden w-full md:block md:w-auto"
@@ -14,12 +16,22 @@ const Navbar = () => {
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-teal-800 md:dark:bg-teal-900 dark:border-gray-700">
                         <li>
                             <div className="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-teal-700 md:p-0 md:dark:hover:text-teal-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent self-center">
-                                <Link to="/login">Login</Link>
+                                <Link
+                                    className="focus:text-teal-200"
+                                    to="/login"
+                                >
+                                    Login
+                                </Link>
                             </div>
                         </li>
                         <li>
                             <div className="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-teal-700 md:p-0 md:dark:hover:text-teal-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent self-center">
-                                <Link to="/table">Table</Link>
+                                <Link
+                                    className="focus:text-teal-200"
+                                    to="/table"
+                                >
+                                    Table
+                                </Link>
                             </div>
                         </li>
                     </ul>
